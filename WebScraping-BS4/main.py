002a656122a74bcd11a0d6f6e8f1a664726ec0f1
HHAIE <hazemhussein@windowslive.com>
2022-12-11 07:52:15 +0100

    Removed dash from last eps file to ease import

--- WebScraping-BS4/main.py
+++ WebScraping-BS4/main.py
@@ -1,4 +1,4 @@
-from last-eps import down_last_eps
+from lastEps import down_last_eps
 
 followed_animes = ["One Piece", "Chainsaw Man", "Mairimashita! Iruma-kun 3rd Season", "Spy x Family Part 2", "Shinobi no Ittoki", "Xian Wang de Richang Shenghuo 3", "Mob Psycho 100 III", "Bleach: Sennen Kessen-hen", "Boku no Hero Academia 6th Season", "Yowamushi Pedal: Limit Break", "Detective Conan"]
 down_last_eps(followed_animes)

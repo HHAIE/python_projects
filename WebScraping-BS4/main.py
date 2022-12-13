6ae4c9b3080555a35458c18a619090bfc6fbddc9
HHAIE <hazemhussein@windowslive.com>
2022-12-13 10:20:07 +0100

    Created the franko to arabic project

--- WebScraping-BS4/main.py
+++ WebScraping-BS4/main.py
@@ -3,16 +3,16 @@ from epsAn4up import down_eps_an4up
 from epsGogo import down_eps_gogo
 
 # Check a list of animes for new episodes and download them if found
-# followed_animes = ["One Piece", "Chainsaw Man", "Mairimashita! Iruma-kun 3rd Season", "Spy x Family Part 2", "Shinobi no Ittoki", "Xian Wang de Richang Shenghuo 3", "Mob Psycho 100 III", "Bleach: Sennen Kessen-hen", "Boku no Hero Academia 6th Season", "Yowamushi Pedal: Limit Break", "Detective Conan"]
-# down_last_eps(followed_animes)
+followed_animes = ["One Piece", "Chainsaw Man", "Mairimashita! Iruma-kun 3rd Season", "Spy x Family Part 2", "Shinobi no Ittoki", "Xian Wang de Richang Shenghuo 3", "Mob Psycho 100 III", "Bleach: Sennen Kessen-hen", "Boku no Hero Academia 6th Season", "Yowamushi Pedal: Limit Break", "Detective Conan"]
+down_last_eps(followed_animes)
 
 
 # Url and name of an anime and number of wanted episodes to download from anime4up
-url = "https://anime4up.tv/anime/gaiken-shijou-shugi/"
-name = "Gaiken Shijou Shugi"
-eps_count = 8
+# url = "https://anime4up.tv/anime/gaiken-shijou-shugi/"
+# name = "Gaiken Shijou Shugi"
+# eps_count = 8
 
-down_eps_an4up(url, name, eps_count)
+# down_eps_an4up(url, name, eps_count)
 
 
 # Url and name of an anime and number of wanted episodes to download from gogoanime
